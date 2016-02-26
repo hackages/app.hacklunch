1c0b9b9d9147658018c8bcd25484d91c50eb7742
davyengone <engonenzedavy@yahoo.com>
2016-02-26 23:40:18 +0100

    added code splitting in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 'use strict';
 
-let path = require('path');
+let path     = require('path');
+let webpack  = require('webpack');
 
 module.exports = {
   entry: './app/index',
   output: {
     filename: 'hacklunch.js',
-    path: path.join(__dirname, 'dist')
+    path: path.join(__dirname, 'dist'),
+    vendor: ['angular']
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js')
+  ],
   module: {
     loaders: [
       {
